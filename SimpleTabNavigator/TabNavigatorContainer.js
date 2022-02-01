a1dd7b45f5c8c34c060419ab23041b32d61dba1f
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2022-02-01 06:20:38 -0600

    added swipe right/left capability to navigate through tabs

--- SimpleTabNavigator/TabNavigatorContainer.js
+++ SimpleTabNavigator/TabNavigatorContainer.js
@@ -3,13 +3,39 @@ import { StyleSheet, View } from 'react-native';
 
 const TabNavigatorContainer = ({ tabsList, children }) => {
     const [currentTab, setCurrentTab] = useState('Profile');
+    const [swipeStart, setSwipeStart] = useState(null);
 
     const updateCurrentTab = (title) => {
         setCurrentTab(title)
     }
 
+    const swipeHandler = (swipeEnd) => {
+        const currentIndex = tabsList.findIndex(tab => tab === currentTab);
+        const lastIndex = tabsList.length - 1;
+
+        if (swipeEnd - swipeStart < 0) {
+            if (currentIndex === lastIndex) {
+                setCurrentTab(tabsList[0]);
+            } else {
+                setCurrentTab(tabsList[currentIndex + 1]);
+            }
+
+        } else {
+            if (currentIndex === 0) {
+                setCurrentTab(tabsList[lastIndex]);
+            } else {
+                setCurrentTab(tabsList[currentIndex - 1]);
+            }
+
+        }
+    };
+
     return (
-        <View style={styles.container}>
+        <View
+            style={styles.container}
+            onTouchStart={(e) => setSwipeStart(e.nativeEvent.pageX)}
+            onTouchEnd={(e) => swipeHandler(e.nativeEvent.pageX)}
+        >
             {React.Children.map(children, (child) => {
                 return (
                     React.cloneElement(child, { tabNavigation: { currentTab, updateCurrentTab, tabsList } })
